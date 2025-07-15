c4e441bfe4a8ced70f6c608e569a132613bf8a94
LaryDiana <larisa.gabor17@gmail.com>
2025-07-15 19:37:56 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,121 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Care era numele complet al împărătesei Sisi?",
+        answer: "C. Elisabeta Amalia Eugenia de Wittelsbach",
+        options: [
+            "A. Maria Theresa de Habsburg",
+            "B. Kathrina von Habsburg",
+            "C. Elisabeta Amalia Eugenia de Wittelsbach",
+            "D. Elisabeta Sophie Charlotte von Bayern",
+        ]
+
+    
+    },
+    {
+        numb: 2,
+        question: "În ce an s-a căsătorit Sisi cu împăratul Franz Joseph?",
+        answer: "B. 1854",
+        options: [
+            "A. 1848",
+            "B. 1854",
+            "C. 1867",
+            "D. 1871",
+        ]
+        
+    },
+    {
+        numb: 3,
+        question:"Ce sport preferat practica Sisi cu pasiune?",
+        answer: "A. Echitație",
+        options: [
+            "A. Echitație",
+            "B. Patinajul pe gheață",
+            "C. Scrimă",
+            "D. Tirul cu arcul",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Cum își îngrijea Sisi părul lung, considerat unul dintre cele mai frumoase din Europa?",
+        answer: "C. Îl spăla cu un amestec de ouă și coniac și petrecea ore întregi pieptănându-l",
+        options: [
+            "A. Îl spăla zilnic cu apă de trandafiri",
+            "B. Îl ascundea mereu sub peruci elaborate",
+            "C. Îl spăla cu un amestec de ouă și coniac și petrecea ore întregi pieptănându-l",
+            "D. Îl tăia scurt la fiecare câțiva ani",
+        ]
+    },
+   
+    {
+        numb: 5,
+        question: "Ce film celebru din anii '50 a popularizat povestea Împărătesei Sisi?",
+        answer: "B. Sisi cu Romy Schneider",
+        options: [
+            "A. Anna Karenina",
+            "B. Sisi cu Romy Schneider",
+            "C. Marie Antoinette",
+            "D. Prințesa Sisi",
+        ]
+    },
+    {
+        numb: 6,
+        question: "Ce tragedie a afectat-o profund pe Sisi în 1889?",
+        answer: "D. Moartea fiului ei, Prințul Rudolf",
+        options: [
+            "A. Asasinarea soțului ei",
+            "B. Moartea mamei sale",
+            "C. Pierderea tronului Austriei",
+            "D. Moartea fiului ei, Prințul Rudolf",
+        ]
+    },
+    {
+        numb: 7,
+        question: "Care a fost reacția împărătesei Sisi față de viața la curtea vieneză?",
+        answer: "A. A urât rigiditatea protocolului și s-a simțit prizonieră",
+        options: [
+            "A. A urât rigiditatea protocolului și s-a simțit prizonieră",
+            "B. A iubit eticheta și s-a adaptat ușor",
+            "C. A devenit o figură influentă la curte",
+            "D. A fost indiferentă și s-a retras în mănăstire",
+        ]
+    },
+    {
+    numb: 8,
+    question: "Cum a fost asasinată Sisi?",
+    answer: "C. A fost înjunghiată de un anarhist italian",
+    options: [
+        "A. A fost împușcată de un revoluționar maghiar",
+        "B. A fost otrăvită de un anarhist italian",
+        "C. A fost înjunghiată de un anarhist italian",
+        "D. A fost otrăvită de un anarhist maghiar",
+    ]
+    },
+    {
+        numb: 9,
+        question: "Cine a fost asasinul împărătesei Sisi?",
+        answer: "B. Luigi Lucheni",
+        options: [
+            "A. Gavrilo Princip",
+            "B. Luigi Lucheni",
+            "C. Alfred Redl",
+            "D. Adolf Loos",
+        ]
+    },
+    {
+        numb: 10,
+        question: "Ce a spus Sisi în momentul în care a fost rănită grav??",
+        answer: "D. Nu am simțit nimic",
+        options: [
+            "A. Nu sunt în pericol",
+            "B. Mă simt slăbită, dar voi trăi",
+            "C. Aș vrea să mă duc acasă",
+            "D. Nu am simțit nimic",
+        ]
+    },
+];
+
+
+
+
+
